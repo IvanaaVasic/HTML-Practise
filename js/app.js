24d7c8c972884bfe22adaa9b7b689d02cdbf6cea
IvanaV <ivana.p.vasic@gmail.com>
2020-04-28 13:30:36 +0200

    container width

--- js/app.js
+++ js/app.js
@@ -9,3 +9,19 @@ function openMenu(menuName) {
 function goBack() {
   window.history.go(-1);
 }
+var myIndex = 0;
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slider-image");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+carousel();
